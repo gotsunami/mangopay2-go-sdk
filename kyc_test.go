d6b4e42acde65ae2837216466286b2fccabfae00
Sergey Yarmonov <sergey.yarmonov@gmail.com>
2016-07-20 18:54:38 +0700

    Add KYC tests

---
+++ kyc_test.go
@@ -0,0 +1,36 @@
+package mango
+
+import (
+	"bytes"
+	"image"
+	"image/png"
+	"testing"
+)
+
+func TestKYC(test *testing.T) {
+	serv, err := newTestService()
+	user := createTestUser(serv)
+	if err := user.Save(); err != nil {
+		test.Fatal("Unable to store user:", err)
+	}
+	doc, err := serv.NewDocument(user, "IDENTITY_PROOF", "Tag1")
+	if err != nil {
+		test.Fatal("Unable to create identity proof doc:", err)
+	}
+	if err := doc.CreatePage(newPngImageFile()); err != nil {
+		test.Fatal("Unable to create document's page:", err)
+	}
+	if err := doc.Submit("VALIDATION_ASKED", "Tag2"); err != nil {
+		test.Fatal("Unable to submit document")
+	}
+}
+
+func newPngImageFile() []byte {
+	img := image.NewRGBA(image.Rect(0, 0, 1024, 768))
+	var buffer bytes.Buffer
+	err := png.Encode(&buffer, img)
+	if err != nil {
+		panic("Unable to create image file: " + err.Error())
+	}
+	return buffer.Bytes()
+}
