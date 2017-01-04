d15ed86fe38330a36acb73a3afdbef367dfc11e5
Sergei Zimakov <zimakov@gmail.com>
2017-01-04 11:30:12 +0300

    PD-1060 Add tests for bank account, payout, refund

---
+++ bank_test.go
@@ -0,0 +1,37 @@
+package mango
+
+import (
+	"testing"
+)
+
+const (
+	testIBAN = "FR3020041010124530725S03383"
+	testBIC  = "CRLYFRPP"
+)
+
+func TestBankAccount_Save(test *testing.T) {
+	serv := newTestService(test)
+	user := createTestUser(serv)
+	if err := user.Save(); err != nil {
+		test.Fatal("Unable to store user:", err)
+	}
+
+	account := createTestBankAccount(test, serv, user)
+
+	if account.IBAN != testIBAN {
+		test.Fatalf("Invalid IBAN is saved bank account: got %s, should be %s", account.IBAN, testIBAN)
+	}
+}
+
+func createTestBankAccount(test *testing.T, serv *MangoPay, user *NaturalUser) *BankAccount {
+	acc, err := serv.NewBankAccount(user, user.FirstName, "one great place", IBAN)
+	if err != nil {
+		test.Fatal("Unable to create BankAccount", err.Error())
+	}
+	acc.IBAN, acc.BIC = testIBAN, testBIC
+	if err := acc.Save(); err != nil {
+		test.Fatal("Unable to save BankAccount", err.Error())
+	}
+
+	return acc
+}
